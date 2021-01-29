79c5c1e19f0762a207bac2af1f775a51cf544483
Ubaid-Ali <syedubaidali2020@gmail.com>
2021-01-30 01:19:18 +0500

    delete old storybook create Task.js and Task.stories.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          App Component
-      </header>
+      <header className="App-header">App Component</header>
     </div>
   );
 }
